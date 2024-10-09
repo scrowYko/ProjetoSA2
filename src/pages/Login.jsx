ae8894e1c8befb674d17466aa2b6655de4ebde8e
DAVI HENRY MOREL PINTOS <davi_pintos@estudante.sesisenai.org.br>
2024-10-09 12:07:53 -0300

    a

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -45,7 +45,7 @@ export default function Login() {
           </form>
           <p
             className="signinEsquecer"
-            onClick={() => navigate("/EsqueciASenha")}
+            onClick={() => navigate("/RecuperarSenha")}
           >
             Esqueci a Senha
           </p>
