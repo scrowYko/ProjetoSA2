8588555e370b183043313d83e770b7e181ca69bf
NicolasGarsia <nicolaslapolli@gmail.com>
2024-10-10 20:52:04 -0300

    socorro2

--- src/components/HomeC.jsx
+++ src/components/HomeC.jsx
@@ -13,7 +13,7 @@ export default function HomeC({ TITULO, CONTAGEM, TIPOC, SITUC }) {
           <h1 className="cTitulos">Diponíveis:</h1>
           </div>
         <div className="boxC">
-          
+          <img src="./trofeu.png"/>
           <div className="boxET">
             <h2 className="nomeT">{TITULO}</h2>
             <div className="txt">
@@ -23,55 +23,127 @@ export default function HomeC({ TITULO, CONTAGEM, TIPOC, SITUC }) {
           </div>
 
           <div className="boxBC">
-            <button> Participe </button>
-            <p>{CONTAGEM}</p>
+            <button className="btn"> Participe </button>
+            <p className="ctgm">{CONTAGEM}</p>
+          </div>
+
+        </div>
+        <div className="boxC">
+        <img src="./trofeu.png"/>
+          <div className="boxET">
+            <h2 className="nomeT">{TITULO}</h2>
+            <div className="txt">
+              <p>Tipo: {TIPOC}</p>
+              <p>Situação: {SITUC}</p>
+            </div>
+          </div>
+
+          <div className="boxBC">
+            <button className="btn"> Participe </button>
+            <p className="ctgm">{CONTAGEM}</p>
+          </div>
+
+        </div>
+        <div className="boxC">
+        <img src="./trofeu.png"/>
+          <div className="boxET">
+            <h2 className="nomeT">{TITULO}</h2>
+            <div className="txt">
+              <p>Tipo: {TIPOC}</p>
+              <p>Situação: {SITUC}</p>
+            </div>
+          </div>
+
+          <div className="boxBC">
+            <button className="btn"> Participe </button>
+            <p className="ctgm">{CONTAGEM}</p>
+          </div>
+
+        </div>
+        <div className="boxC">
+        <img src="./trofeu.png"/>
+          <div className="boxET">
+            <h2 className="nomeT">{TITULO}</h2>
+            <div className="txt">
+              <p>Tipo: {TIPOC}</p>
+              <p>Situação: {SITUC}</p>
+            </div>
+          </div>
+
+          <div className="boxBC">
+            <button className="btn"> Participe </button>
+            <p className="ctgm">{CONTAGEM}</p>
           </div>
 
         </div>
 </div>
 
-
-      <div>
-        <div className="box2">
-
-          <div className="boxET">
-            <h2 className="nomeT">TITULO DO TIME</h2>
-            <div className="txt">
-              <p></p>
-              <p></p>
-            </div>
-          </div>
-
-          <div>
-            <p> Finalizado </p>
-            <p>Contagem participantes</p>
-          </div>
-
-        </div>
-
-
-        <div className="box2">
-
-          <h1 className="cTitulos"> Campeonatos Ativos</h1>
-
-          <div className="boxET">
-            <h2 className="nomeT">TITULO DO TIME</h2>
-            <div className="txt">
-              <p></p>
-              <p></p>
-            </div>
-          </div>
-
-          <div>
-            <p> Finalizado </p>
-            <p>Contagem participantes</p>
-          </div>
-
-        </div>
-
-
-
+<div className="boxTD2">
+<div className="boxTd">
+  <div>
+    <h1 className="cTitulos">Campeonatos Ativos:</h1>
+    </div>
+  <div className="boxC">
+  <img src="./trofeu.png"/>
+    <div className="boxT">
+      <h2 className="nomeT">Campeonato Holandes Eredivisie:</h2>
+      <div className="txt">
+        <p>Tipo: Pontos corrido</p>
+        <p>Situação: Encerrado</p>
       </div>
+    </div>
+
+    <div className="boxBC">
+      <p className="btn2"> Finalizado </p>
+      <p className="ctgm">10/10</p>
+    </div>
+    
+
+  </div>
+  <div className="boxC">
+  <img src="./trofeu.png"/>
+    <div className="boxT">
+      <h2 className="nomeT">Champions League</h2>
+      <div className="txt">
+        <p>Tipo: {TIPOC}</p>
+        <p>Situação: Encerrado</p>
+      </div>
+    </div>
+
+    <div className="boxBC">
+      <p className="btn2"> Finalizado </p>
+      <p className="ctgm">10/10</p>
+    </div>
+    
+
+  </div>
+  
+  </div>
+
+
+
+  <div className="boxTd">
+  <div>
+    <h1 className="cTitulos">Resultados Recentes:</h1>
+    </div>
+  <div className="boxC">
+  <img src="./trofeu.png"/>
+    <div className="boxET">
+      <h2 className="nomeT">{TITULO}</h2>
+      <div className="txt">
+        <p>Tipo: {TIPOC}</p>
+        <p>Situação: <p>{SITUC}</p></p>
+      </div>
+    </div>
+
+    <div className="boxBC">
+      <button className="btn"> Participe </button>
+      <p className="ctgm">{CONTAGEM}</p>
+    </div>
+
+  </div>
+  </div>
+  </div>
     </div>
   );
 }
